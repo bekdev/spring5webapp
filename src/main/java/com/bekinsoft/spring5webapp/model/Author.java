c559936f4ad078b2b03be7f23b43ab4594c7d225
bekdev <bekintest@gmail.com>
2018-10-27 14:43:17 +0100

    updating JPA example

--- src/main/java/com/bekinsoft/spring5webapp/model/Author.java
+++ src/main/java/com/bekinsoft/spring5webapp/model/Author.java
@@ -2,6 +2,7 @@ package com.bekinsoft.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -59,5 +60,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
